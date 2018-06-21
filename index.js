a3e2a2e2bfe35cc09d0425a54f6e2c883eceb2c4
Howard Leung <hwrdleung@gmail.com>
2018-06-20 15:19:08 -1000

    Debug

--- index.js
+++ index.js
@@ -12,26 +12,33 @@ const games = require('./routes/games')(router);
 
 //Database
 const mongoose = require('mongoose');
-mongoose.connect ('mongodb://noodles01:noodles01@ds253879.mlab.com:53879/gametrade');
+mongoose.connect('mongodb://noodles01:noodles01@ds253879.mlab.com:53879/gametrade');
 
 app.use(cors());
+app.all('*', function (req, res, next) {
+    var origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.static(__dirname + '/public/'));
 
 //Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/user', user);
 app.use('/games', games);
 
 //Route
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+app.get('/', (req, res) => {
+    response.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 
 
 //Start server
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
